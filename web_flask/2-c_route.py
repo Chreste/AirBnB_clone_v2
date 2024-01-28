521d2aee9afa335dbf423af141e6e46569e4e70a
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-28 07:32:53 -0800

    done c-route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     " Function to display 'C ' followed by the text variable  "
-    return 'C ' + text.replace('_', ' ')
+    return 'C %s' %  text.replace('_', ' ')
 
 if __name__ == "__main__":
     app.run(host='1.0.0.0', port=5000)
