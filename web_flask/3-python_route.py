d001be182680a526c9cd2d4d95567f6353de1e33
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-28 07:43:25 -0800

    done c-route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-" Sript that starts a Flask web application "
+""" Starts a Flash Web Application Python is Cool"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    " function to return Hello HBNB! "
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    " function to return hbnb! "
+    """ Prints a Message when /hbnb is called """
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -22,4 +29,5 @@ def python_is_cool(text='is_cool'):
     return "Python " + text.replace('_', ' ')
 
 if __name__ == "__main__":
-    app.run(host='1.0.0.0', port=5000)
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
